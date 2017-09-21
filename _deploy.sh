adf58a193f37afd8b32844831ff3242634b06c26
amatsuo <matsuoakitaka@gmail.com>
2017-09-21 11:36:50 +0100

    update travis settings

--- _deploy.sh
+++ _deploy.sh
@@ -5,10 +5,10 @@ set -e
 #[ -z "${GITHUB_PAT}" ] && exit 0
 #[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "kbenoit@lse.ac.uk"
-git config --global user.name "Travis CI"
+# git config --global user.email "kbenoit@lse.ac.uk"
+# git config --global user.name "Travis CI"
 
-git clone -b gh-pages git@github.com:kbenoit/coursepack-bookdown.git book-output
+git clone -b gh-pages git@github.com:LSE-Methodology/MY451.git book-output
 cd book-output
 cp -r ../_book/* ./
 git add --all *
