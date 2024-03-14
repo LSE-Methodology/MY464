ac43eb2e1541b21f3e3b9548eb4d35b3b3bebc6d
Patrick Gildersleve <22916176+pgilders@users.noreply.github.com>
2024-03-14 22:51:15 +0000

    Troubleshooting Travis CI 6
    Travis threw error when creating epub:
    
    Error: Using anchor_sections requires Pandoc 2.0+

--- _build.sh
+++ _build.sh
@@ -2,5 +2,4 @@
 
 cp ./images/* .
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
